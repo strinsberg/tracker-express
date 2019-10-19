95ea3e4a957c20d39d593f8e68383a22cbe258f8
deus3720 <deutekom@uleth.ca>
2019-10-19 13:41:31 -0600

    added some simple implementation to the server main.

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,9 +1,19 @@
 #include <restbed>
 #include <nlohmann/json.hpp>
-#include <iostream>
+#include <memory>
+
 
 int main() {
-    std::cout << "Server Is ON!!" << std::endl;
+    auto resource = std::make_shared<restbed::Resource>();
+    resource->set_path("/trackexp");
+    //resource->set_method_handler("GET", );
 
-    return 0;
+    auto settings = std::make_shared<restbed::Settings>();
+    settings->set_port(1234);
+
+    restbed::Service service;
+    service.publish(resource);
+
+    service.start(settings);
+    return EXIT_SUCCESS;
 }
