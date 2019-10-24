c58314bae87273e485bc4a0a0d39b2625e146240
deus3720 <deutekom@uleth.ca>
2019-10-23 22:59:28 -0600

    Pulled in the files that I did not merge correctly. I thnk everything is proper now.

--- include/IssueSystem.h
+++ include/IssueSystem.h
@@ -1,61 +1,104 @@
 #ifndef ISSUE_SYSTEM_H
 #define ISSUE_SYSTEM_H
 
-#include <string>
-#include <vector>
 #include "User.h"
 #include "Issue.h"
+#include "Comment.h"
+#include <string>
+#include <vector>
 
 /**
  * Holds the Issues, and Users for the issue tracking system.
  *
- * @author Steven Deutekom
+ * @author Steven Deutekom, Mathew Richards
  * @date Oct 19 2019
  */
 class IssueSystem {
  public:
-  /**
-   * Create a new issue system.
-   */
-  IssueSystem();
+    /**
+     * Create a new issue system.
+     */
+    IssueSystem();
 
-  virtual IssueSystem();
+    virtual ~IssueSystem();
 
-  /**
-   * Get all the issues.
-   * 
-   * @return the list of issues.
-   */
-  std::vector<Issues>& getIssues();
+    /**
+     * Get all the issues.
+     *
+     * @return the list of issues.
+     */
+    std::vector<Issue>& getIssues();
 
-  /**
-   * Get all the users.
-   *
-   * @return the list of users.
-   */
-  std::vector<User>& getUsers();
+    /**
+     * Get all the users.
+     *
+     * @return the list of users.
+     */
+    std::vector<User>& getUsers();
 
-  /**
-   * Gets an issue by ID.
-   *
-   * @param id The ID of the issue.
-   * @return the issue for the id
-   * @throws invalid_argument if the ID does not exist.
-   */
-  Issue& getIssue(int id);
+    /**
+     * Get all comments.
+     *
+     * @return the list of comments.
+     */
+    std::vector<Comment>& getComments();
 
-  /**
-   * Get a player by ID.
-   *
-   * @param id The ID of the player.
-   * @return the player with the ID.
-   * @throws invalid_argument if the ID does not exist.
-   */
-  User& getUser(int id);
+    /**
+     * Gets an issue by ID.
+     *
+     * @param id The ID of the issue.
+     * @return the issue for the id
+     * @throws invalid_argument if the ID does not exist.
+     */
+    Issue& getIssue(int id);
+
+    /**
+     * Get a player by ID.
+     *
+     * @param id The ID of the player.
+     * @return the player with the ID.
+     * @throws invalid_argument if the ID does not exist.
+     */
+    User& getUser(int id);
+
+    /**
+     * Get a comment by ID.
+     *
+     * @param id The id of the comment.
+     * @return return the comment by ID.
+     * @throws invalid_argument if the ID does not exist.
+     */
+    Comment& getComment(int id);
+
+    /**
+     * Create a new issue.
+     *
+     * @return the id of the issue.
+     */
+    int createIssue();
+
+    /**
+     * Create a user.
+     *
+     * @return the id of the user.
+     */
+    int createUser();
+
+    /**
+     * Create a new comment.
+     *
+     * @return the id of the comment.
+     */
+    int createComment();
 
  private:
-  std::vector<Issues> issues;
-  std::vector<Users> users;
+    std::vector<Issue> issues;
+    std::vector<User> users;
+    std::vector<Comment> comments;
+
+    int issueCount;
+    int commentCount;
+    int userCount;
 };
 
 #endif
