d17ddeead4321c99f2d2d05ff303832ac076ea30
deus3720 <deutekom@uleth.ca>
2019-10-23 21:52:56 -0600

    Grabbed changes for the issue class since I need them to make this branch work again.

--- test/TestIssue.cpp
+++ test/TestIssue.cpp
@@ -1,40 +1,45 @@
-#include "Issue.h"
 #include "gtest/gtest.h"
+#include "Issue.h"
 
 TEST(TestIssue, testTitle) {
-Issue i;
+Issue i(1);
 i.setTitle("Test");
 EXPECT_EQ("Test", i.getTitle());
 }
 
 TEST(TestIssue, testDescription) {
-Issue i;
+Issue i(1);
 i.setDescription("Test");
 EXPECT_EQ("Test", i.getDescription());
 }
 
 TEST(TestIssue, testAssignee) {
-Issue i;
+Issue i(1);
 i.setAssignee(0);
 EXPECT_EQ(0, i.getAssignee());
 }
 
 TEST(TestIssue, testTag) {
-Issue i;
+Issue i(1);
 i.addTag("Test");
 EXPECT_EQ("Test", i.getTagAtPos(0));
 }
 
 TEST(TestIssue, testTag2) {
-Issue i;
+Issue i(1);
 i.addTag("Test");
 EXPECT_EQ("Test", i.getTags().at(0));
 }
 
 TEST(TestIssue, testPriority) {
-Issue i;
+Issue i(1);
 i.setPriority(1);
 EXPECT_EQ(1, i.getPriority());
+}
+
+TEST(TestIssue, testIdGetter) {
+Issue i(1);
+EXPECT_EQ(1, i.getIssueId());
 }
 
 /*
