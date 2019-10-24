6229abe5224f2e7a4662bcbe99467a124c5bb0c4
deus3720 <deutekom@uleth.ca>
2019-10-24 13:39:59 -0600

    added some more for the post with issues. not finished yet.

--- src/service/Handlers.cpp
+++ src/service/Handlers.cpp
@@ -2,6 +2,7 @@
 #include <nlohmann/json.hpp>
 #include "Handlers.h"
 #include "IssueSystem.h"
+#include "Status.h"
 
 
 #define ALLOW_ALL { "Access-Control-Allow-Origin", "*" }
@@ -34,4 +35,40 @@ void Handlers::get_issues(const std::shared_ptr<restbed::Session>& session,
       { "Content-Length", std::to_string(response.length()) },
       CLOSE_CONNECTION
   });
+}
+
+void Handlers::create_issue(const std::shared_ptr<restbed::Session>& session,
+                            const restbed::Bytes& body,
+                            IssueSystem& system) {
+  const auto request = session->get_request();
+  size_t content_length = request->get_header("Content-Length", 0);
+  
+  session->fetch(content_length, [&]() {
+    int id = system.createIssue();
+    Issue& iss = system.getIssue(id);
+    
+    auto data = nlohmann::json::parse(body.data());
+
+    iss.setTitle(data["title"]);
+    iss.setDescription(data["description"]);
+    iss.setAssignee(data["assignee"]);
+    iss.setCreator(data["creator"]);
+    iss.setPriority(data["priority"]);
+    if (iss.getAssignee() == -1)
+        iss.setStatus(Status::NEW);
+    else
+        iss.setStatus("Assigned");
+    
+    nlohmann::json result = {
+      {"status", "ok"},
+      {"response", {"id", id}}
+    };
+    std::string response = result.dump();
+
+    session->close(restbed::OK, response, {
+      ALLOW_ALL, {
+        "Content-Length", std::to_string(response.length())
+      },
+      CLOSE_CONNECTION
+    });
 }
