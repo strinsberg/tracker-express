fd9abdafb4f2194841f5fce7e3a43d61be80d64d
deus3720 <deutekom@uleth.ca>
2019-11-12 14:44:26 -0700

    Setup handler implementation to use the new toJson functions and creation functions that take JSON strings. Tested by running server since gtest does not work with 9.1 yet, but this change moved most of the logic to the model classes.

--- src/server/Handlers.cpp
+++ src/server/Handlers.cpp
@@ -23,14 +23,9 @@ void Handlers::get_issues(const std::shared_ptr<restbed::Session>& session,
 
   std::cout << "GET: Issues:" << std::endl;
   for (auto & iss : system->getIssues()) {
-    nlohmann::json entryJSON = {
-        {"id", iss.getId()},
-        {"title", iss.getTitle()},
-        {"created", iss.getCreator()},
-        {"priority", iss.getPriority()},
-    };
-    responseJSON["response"].push_back(entryJSON.dump());
-    std::cout << entryJSON.dump() << std::endl;
+    std::string json(iss.toJson().dump());
+    responseJSON["response"].push_back(json);
+    std::cout << json << std::endl;
   }
 
   std::string response = responseJSON.dump();
@@ -51,25 +46,11 @@ void Handlers::create_issue(const std::shared_ptr<restbed::Session>& session,
   session->fetch(content_length,
       [system](const std::shared_ptr<restbed::Session>& session,
           const restbed::Bytes& body) {
-    int id = system->createIssue();
-    Issue& iss = system->getIssue(id);
-
-    std::cout << "Body: " << body.data() << std::endl;
-    auto data = nlohmann::json::parse(body.data());
-
-    iss.setTitle(data["title"]);
-    iss.setDescription(data["description"]);
-    iss.setAssignee(data["assignee"]);
-    iss.setCreator(data["creator"]);
-    iss.setPriority(data["priority"]);
-    if (iss.getAssignee() == -1)
-        iss.setStatus(Status::NEW);
-    else
-        iss.setStatus(Status::ASSIGNED);
+    Issue& iss = system->createIssue(reinterpret_cast<const char*>(body.data()));
 
     nlohmann::json result = {
       {"status", "ok"},
-      {"response", data}
+      {"response", iss.toJson().dump()}
     };
     std::string response = result.dump();
 
@@ -96,14 +77,9 @@ void Handlers::get_users(const std::shared_ptr<restbed::Session>& session,
 
   std::cout << "GET: Users:" << std::endl;
   for (auto & user : system->getUsers()) {
-    nlohmann::json entryJSON = {
-        {"id", user.getId()},
-        {"name", user.getName()},
-        {"blurb", user.getBlurb()},
-        {"pic", user.getPictureNum()},
-    };
-    responseJSON["response"].push_back(entryJSON.dump());
-    std::cout << entryJSON.dump() << std::endl;
+    std::string json(user.toJson().dump());
+    responseJSON["response"].push_back(json);
+    std::cout << json << std::endl;
   }
 
   std::string response = responseJSON.dump();
@@ -125,31 +101,12 @@ void Handlers::create_user(const std::shared_ptr<restbed::Session>& session,
   session->fetch(content_length,
       [system](const std::shared_ptr<restbed::Session>& session,
           const restbed::Bytes& body) {
-    int id = system->createUser();
-    User& user = system->getUser(id);
-
-    nlohmann::json data;
-    std::string temp_body(reinterpret_cast<const char*>(body.data()));
-
-    // Hack to remove extra info from post body
-    // Don't know why it is there, but it should be fixed eventually
-    std::cout << "Body: " << temp_body.substr(0, temp_body.length() - 8);
-    std::cout << std::endl;
-    if (temp_body.find("/users'") != std::string::npos)
-        data = nlohmann::json::parse(
-          temp_body.substr(0, temp_body.length() - 8));
-    else
-        data = nlohmann::json::parse(temp_body);
-
-    // Setup user object
-    user.setName(data["name"]);
-    user.setBlurb(data["blurb"]);
-    user.setPictureNum(data["pic"]);
+    User& user = system->createUser(reinterpret_cast<const char*>(body.data()));
 
     // Create and send response
     nlohmann::json result = {
       {"status", "ok"},
-      {"response", data}
+      {"response", user.toJson().dump()}
     };
     std::string response = result.dump();
 
