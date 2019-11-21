170921e8b80141e0f4a784ca5ac6e4664571cd1a
deus3720 <deutekom@uleth.ca>
2019-11-20 19:12:29 -0700

    Made some changes and updates to the way comments work and are displayed. Will still need work, but comments are added to a table on the page.

--- src/web/js/singleIssue.js
+++ src/web/js/singleIssue.js
@@ -15,21 +15,35 @@ fetch("http://localhost:1234/trackEx/issues?id=" + params.get("id"))
     document.getElementById("tags").innerHTML = "Tags: " + iss.tags;
 });
 
-fetch("http://localhost:1234/trackEx/comments?id=" + params.get("id"))
+fetch("http://localhost:1234/trackEx/comments")
 .then(response => {
+    console.log(response);
     return response.json();
 })
 .then(data => {
-  
+    var table = document.getElementById("comment_table");
+    data.response.forEach(comment => {
+        var com = JSON.parse(comment);
+        var row = table.insertRow(-1);
+        
+        var user = row.insertCell(0);
+        var text = row.insertCell(1);
+        
+        user.innerHTML = com.user_id;
+        text.innerHTML = com.text;
+
+        console.log("Comment:", com);
+    });
+}).catch(err => {
+    console.error("Error:", err);
 });
 
-//not working properly yet
-//i can confirm the handler works because I tested it with curl
+
 async function deleteIssue() {
-if (confirm("Are you sure you would like to delete this issue?")) {
-fetch("http://localhost:1234/trackEx/issues?id=" + params.get("id") + "&delete")
-window.open("issues.html","_self");
-}
+    if (confirm("Are you sure you would like to delete this issue?")) {
+        fetch("http://localhost:1234/trackEx/issues?id=" + params.get("id") + "&delete")
+        window.open("issues.html","_self");
+    }
 }
 
 async function postComment() {
@@ -38,7 +52,7 @@ async function postComment() {
     "issue_id": parseInt(params.get("id")),
     "user_id": parseInt(document.getElementById("userID").value)
   };
-  console.log(values);
+  //console.log(values);
 
   fetch("http://localhost:1234/trackEx/comments", {
     method: "POST",
@@ -47,6 +61,7 @@ async function postComment() {
       "Content-Type": "text/plain"
     },
     body: JSON.stringify(values),
-  });
-  //We need to change the content on the page dynamically, if the comment is added.
+  })
+  
+  //window.open("singleIssue.html?id=" + params.get("id"), "_self");
 }
