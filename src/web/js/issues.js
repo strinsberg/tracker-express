740ff5dcdd06e5be71be8cfa335686d4bc97b8c1
deus3720 <deutekom@uleth.ca>
2019-11-24 14:28:15 -0700

    Default values for the filtering are setup properly. Actually calling the  filter api is still commented out until server is working.

--- src/web/js/issues.js
+++ src/web/js/issues.js
@@ -1,26 +1,6 @@
-fetch("http://localhost:1234/trackEx/issues") 
-    .then(response => {
-    console.log(response);
-    return response.json();
-    })
-    .then(data => {
-        var table = document.getElementById("table_body");
-        data.response.forEach(issue => {
-            var iss = JSON.parse(issue)
-            var row = table.insertRow(-1);
-            
-            var title = row.insertCell(0);
-            var create = row.insertCell(1);
-            var prior = row.insertCell(2);
-            
-            title.innerHTML = '<a href="singleIssue.html?id=' + iss.id + '">' + iss.title + '</a>';
-            create.innerHTML = iss.creator;
-            prior.innerHTML = iss.priority;
-        })
-    })
-    .catch(err => {
-    console.error("Error:", err);
-});
+window.onload = function() {
+    getIssues("http://localhost:1234/trackEx/issues");
+}
 
 function filterIssues() {
     var priority = document.getElementById("priority-filter");
@@ -28,11 +8,23 @@ function filterIssues() {
     var status = document.getElementById("status-filter");
     
     
-    var url = "http://localhost:1234/trackEx/issues"//?status=" + status.value;
-
+    var url = "http://localhost:1234/trackEx/issues";
+    if (status.value != -1) {
+        url += "&status=" + status.value;
+    }
+    
+    if (priority.value >= 0) {
+        url += "&priority=" + priority.value;
+    }
+    
+    if (tag.value != "") {
+        url+= "&tag=" + tag.value;
+    }
+    
     console.log(url);
     
-    getIssues(url);
+    // Uncomment this when the filter function is working on the server
+    //getIssues(url);
 }
 
 function getIssues(url) {
